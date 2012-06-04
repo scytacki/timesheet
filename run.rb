338cf1dbdebca4ec14a381c31f0bc577e0e1e649
Scott Cytacki <scytacki@concord.org>
2012-06-04 15:20:22 -0400

    DRY up this file since commands are now unified

--- run.rb
+++ run.rb
@@ -4,13 +4,13 @@ require 'date'
 require 'fileutils'
 
 # figure out which month this for
-date = Date.civil(2012, 4);
-month = date.strftime("%b").downcase;
+$date = Date.civil(2012, 5);
+$month = $date.strftime("%b").downcase;
 
 # set the output directory
 root_dir = "/Users/scytacki/Documents/CCProjects/Timesheets/"
-out_dir = root_dir + "#{month}-#{date.year}"
-FileUtils.mkpath out_dir
+$out_dir = root_dir + "#{$month}-#{$date.year}"
+FileUtils.mkpath $out_dir
 
 def run_cmd(cmd)
   cmd = "ruby ../ical-from-x/" + cmd
@@ -18,28 +18,19 @@ def run_cmd(cmd)
   system cmd
 end
 
-# run ical-from-system-events
-system_sleep_ical="#{out_dir}/system_sleep.ics"
-run_cmd "ical-from-system.rb -y #{date.year} -m #{date.mon} -o '#{system_sleep_ical}'"
+def run_metric(metric_name)
+  run_cmd "ical-from-#{metric_name}.rb -y #{$date.year} -m #{$date.mon} -o '#{$out_dir}/#{metric_name}.ics'"
+end
 
-# copy the current chrome history file
-chrome_history_db="#{out_dir}/ch-history.sqlite"
-FileUtils.copy "#{ENV['HOME']}/Library/Application Support/Google/Chrome/Default/History", chrome_history_db
-# run ical_from_chrome-history passing the appropriate options
-chrome_history_ical="#{out_dir}/ch-history.ics"
-run_cmd "ical-from-chrome-history.rb -y #{date.year} -m #{date.mon} -d '#{chrome_history_db}' -o '#{chrome_history_ical}'"
 
-# run ical-from-skype-calls
-skype_calls_ical="#{out_dir}/skype-calls.ics"
-run_cmd "skype/ical-from-skype-calls.rb -o '#{skype_calls_ical}'"
-
-# run ical-from-sent-email
-sent_mail_ical="#{out_dir}/sent-mail.ics"
-run_cmd "ical-from-sent-email.rb -y #{date.year} -m #{date.mon} -o '#{sent_mail_ical}'"
+run_metric 'system-sleep'
+run_metric 'chrome-history'
+run_metric 'skype-calls'
+run_metric 'sent-email'
 
 # the google calendar is setup to sync in iCal if I just refresh it so I don't need to write a script for that
 
 # print out directions for what to do next
 puts "Now clear the existing calendars from ical"
 puts "Then run this command"
-puts "  open #{out_dir}/*.ics"
+puts "  open #{$out_dir}/*.ics"
